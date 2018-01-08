8b2b5a6bff78d83b85e3d1204800564b5a7e4f96
Alex Grintsvayg <git@grin.io>
2018-01-08 15:13:07 -0500

    errors

---
+++ README.md
@@ -0,0 +1,4 @@
+# errors
+
+Better error handling. Marries [go-errors/errors](https://github.com/go-errors/errors) to [pkg/errors](https://github.com/pkg/errors), and 
+adds a little bit of our own magic sauce.
---
+++ errors.go
@@ -0,0 +1,48 @@
+package errors
+
+import (
+	"fmt"
+
+	"github.com/go-errors/errors"
+)
+
+// Err intelligently creates/handles errors, while preserving the stack trace.
+// It works with errors from github.com/pkg/errors too.
+func Err(err interface{}, fmtParams ...interface{}) error {
+	if err == nil {
+		return nil
+	}
+
+	type causer interface {
+		Cause() error
+	}
+
+	if _, ok := err.(causer); ok {
+		err = fmt.Errorf("%+v", err)
+	} else if errString, ok := err.(string); ok && len(fmtParams) > 0 {
+		err = fmt.Errorf(errString, fmtParams...)
+	}
+
+	return errors.Wrap(err, 1)
+}
+
+// Is compares two wrapped errors to determine if the underlying errors are the same
+func Is(e error, original error) bool {
+	return errors.Is(e, original)
+}
+
+// Prefix prefixes the message of the error with the given string
+func Prefix(prefix string, err interface{}) error {
+	if err == nil {
+		return nil
+	}
+	return errors.WrapPrefix(Err(err), prefix, 0)
+}
+
+// Trace returns the error message and the stack trace
+func Trace(err error) string {
+	if err == nil {
+		return ""
+	}
+	return errors.Wrap(Err(err), 0).ErrorStack()
+}
