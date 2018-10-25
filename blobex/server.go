92e35bd23a950a0545c3a869f33d4ea4b2d4c043
Alex Grintsvayg <git@grin.io>
2018-10-25 14:20:21 -0400

    blobex draft

---
+++ blobex/server.go
@@ -0,0 +1,49 @@
+package blobex
+
+import (
+	"fmt"
+	"net"
+
+	"github.com/lbryio/lbry.go/errors"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+type Server struct {
+	pricePerKB uint64
+}
+
+func ListenAndServe(port int) (*grpc.Server, error) {
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		return nil, errors.Prefix("failed to listen", err)
+	}
+	grpcServer := grpc.NewServer()
+	RegisterBlobExchangeServer(grpcServer, &Server{})
+	// determine whether to use TLS
+	err = grpcServer.Serve(listener)
+	return grpcServer, err
+}
+
+func (s *Server) PriceCheck(ctx context.Context, r *PriceCheckRequest) (*PriceCheckResponse, error) {
+	return &PriceCheckResponse{
+		DeweysPerKB: s.pricePerKB,
+	}, nil
+}
+
+func (s *Server) DownloadCheck(context.Context, *HashesRequest) (*HashesResponse, error) {
+	return nil, nil
+}
+
+func (s *Server) Download(BlobExchange_DownloadServer) error {
+	return nil
+}
+
+func (s *Server) UploadOffer(context.Context, *HashesRequest) (*HashesResponse, error) {
+	return nil, nil
+}
+
+func (s *Server) Upload(BlobExchange_UploadServer) error {
+	return nil
+}
