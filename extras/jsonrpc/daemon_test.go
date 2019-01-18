5ef9a229b6ff6ed1c0af8da6f8a10fff1a2470a7
Niko Storni <niko@lbry.io>
2019-01-18 19:10:23 +0100

    add more SDK calls
    export jsonrpc.Decode
    add dep as dependencies manager

--- extras/jsonrpc/daemon_test.go
+++ extras/jsonrpc/daemon_test.go
@@ -5,6 +5,9 @@ import (
 	"strconv"
 	"testing"
 
+	"github.com/lbryio/lbry.go/extras/util"
+
+	"github.com/shopspring/decimal"
 	log "github.com/sirupsen/logrus"
 )
 
@@ -42,6 +45,62 @@ func TestClient_AccountFund(t *testing.T) {
 		t.Error(err)
 	}
 	got, err := d.AccountFund(account, account, fmt.Sprintf("%f", balance-0.1), 40)
+	if err != nil {
+		t.Error(err)
+	}
+	log.Infof("%+v", *got)
+}
+
+func TestClient_AddressUnused(t *testing.T) {
+	d := NewClient("")
+	got, err := d.AddressUnused(nil)
+	if err != nil {
+		t.Error(err)
+	}
+	log.Infof("%s", *got)
+}
+
+func TestClient_ChannelList(t *testing.T) {
+	d := NewClient("")
+	got, err := d.ChannelList(nil, 0, 50)
+	if err != nil {
+		t.Error(err)
+	}
+	log.Infof("%+v", *got)
+}
+
+func TestClient_Publish(t *testing.T) {
+	d := NewClient("")
+	addressResponse, err := d.AddressUnused(nil)
+	if err != nil {
+		t.Error(err)
+	}
+	address := string(*addressResponse)
+	got, err := d.Publish("test", "/home/niko/test.txt", 13.37, PublishOptions{
+		Metadata: &Metadata{
+			Fee: &Fee{
+				Currency: "LBC",
+				Amount:   decimal.NewFromFloat(1.0),
+				Address:  &address,
+			},
+			Title:       "This is a Test Title",
+			Description: "My Special Description",
+			Author:      "Niko",
+			Language:    "en",
+			License:     "FREEEEE",
+			LicenseURL:  nil,
+			Thumbnail:   util.PtrToString("https://scrn.storni.info/2019-01-18_16-37-39-098537783.png"),
+			Preview:     nil,
+			NSFW:        false,
+			Sources:     nil,
+		},
+		ChannelName:      nil,
+		ChannelID:        nil,
+		ChannelAccountID: nil,
+		AccountID:        nil,
+		ClaimAddress:     &address,
+		ChangeAddress:    &address,
+	})
 	if err != nil {
 		t.Error(err)
 	}
