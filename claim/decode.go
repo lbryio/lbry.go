2d240b1604c69c913686d5b7951202864811caee
Niko Storni <niko@lbry.io>
2019-04-03 00:44:30 +0200

    Update to ALMOST support 0.35 with new metadata

--- claim/decode.go
+++ claim/decode.go
@@ -3,7 +3,7 @@ package claim
 import (
 	"bytes"
 
-	types "github.com/lbryio/types/v1/go"
+	types "github.com/lbryio/types/v2/go"
 
 	"github.com/golang/protobuf/jsonpb"
 	"github.com/golang/protobuf/proto"
