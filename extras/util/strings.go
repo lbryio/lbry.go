87bf89a109b48a379b16e4cc3fa744c337b7817b
Jeffrey Picard <jeff@jeffreypicard.com>
2021-06-23 15:29:23 -0400

    Cleanup utility functions and add comments

--- extras/util/strings.go
+++ extras/util/strings.go
@@ -16,41 +16,36 @@ func StringSplitArg(stringToSplit, separator string) []interface{} {
 	return splitInterface
 }
 
-func Normalize(s string) string {
+// NormalizeName Normalize names to remove weird characters and account to capitalization
+func NormalizeName(s string) string {
 	c := cases.Fold()
 	return c.String(norm.NFD.String(s))
 }
-
-func ReverseBytes(s []byte) {
+// ReverseBytesInPlace reverse the bytes. thanks, Satoshi 😒
+func ReverseBytesInPlace(s []byte) {
 	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
 		s[i], s[j] = s[j], s[i]
 	}
 }
 
-// convert txid to txHash
-func ToHash(txid string) []byte {
+// TxIdToTxHash convert the txid to a hash for returning from the hub
+func TxIdToTxHash(txid string) []byte {
 	t, err := hex.DecodeString(txid)
 	if err != nil {
 		return nil
 	}
 
-	// reverse the bytes. thanks, Satoshi 😒
-	for i, j := 0, len(t)-1; i < j; i, j = i+1, j-1 {
-		t[i], t[j] = t[j], t[i]
-	}
+	ReverseBytesInPlace(t)
 
 	return t
 }
 
-// convert txHash to txid
-func FromHash(txHash []byte) string {
+// TxHashToTxId convert the txHash from the response format back to an id
+func TxHashToTxId(txHash []byte) string {
 	t := make([]byte, len(txHash))
 	copy(t, txHash)
 
-	// reverse the bytes. thanks, Satoshi 😒
-	for i, j := 0, len(txHash)-1; i < j; i, j = i+1, j-1 {
-		txHash[i], txHash[j] = txHash[j], txHash[i]
-	}
+	ReverseBytesInPlace(t)
 
 	return hex.EncodeToString(t)
 
